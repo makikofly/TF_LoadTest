36bfd839d564e78ed9b36004f17544c03644b6bc
chen.fei <chen.fei@denachina.com>
2013-01-23 18:06:49 +0800

    fix bug

--- util/kill_test.sh
+++ util/kill_test.sh
@@ -1,5 +1,7 @@
-NUM=`ps aux | grep 'serverPort=8002' | grep -v 'grep' | wc -l`
-if [$NUM -gt 0] ; then
+NUM=`ps aux | grep 'redisServerLoadTest' | grep -v 'grep' | wc -l`
+if [ $NUM -gt 0 ] ; then
 	echo "Running pocess found!"
 	kill `ps aux | grep 'redisServerLoadTest' | grep -v 'grep' | awk '{print $2}'`
+else
+	echo "Process not found."
 fi
